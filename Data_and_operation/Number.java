19f10353eb2b77eefb6cb2a03d3866b36ec96657
jhyeok0310 <98368206+jhyeok0310@users.noreply.github.com>
2022-11-12 23:50:31 +0900

    Add files via upload

---
+++ Data_and_operation/Number.java
@@ -0,0 +1,16 @@
+public class Number {
+
+	public static void main(String[] args) {
+		//operator
+		System.out.println(6 + 2);//8
+		System.out.println(6 - 2);//4
+		System.out.println(6 * 2);//12
+		System.out.println(6 / 2);//3
+		
+		System.out.println(Math.PI);//3.141592...
+		System.out.println(Math.floor(Math.PI));//내림
+		System.out.println(Math.ceil(Math.PI));//올림
+
+	}
+
+}
---
+++ Step1/helloworld/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author tm769
+ *
+ */
+module HelloWorld {
+}
